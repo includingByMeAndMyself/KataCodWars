e5227e96b0ded40faf611c2bbe19c71226b885bd
IvanSminrov <includingivansmirnov@gmail.com>
2022-03-03 09:13:28 +0500

    New Kata6kyu

---
+++ KataCodWars/Kata/Kata.6kyu/Kata7.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KataCodWars.Kata.Kata6kyu
+{
+    public class Kata7
+    {
+        public static int GetUnique(IEnumerable<int> numbers)
+        {
+            foreach (var number in numbers.GroupBy(c => c).Where(g => g.Count() == 1))
+            {
+                return number.Key;
+            }
+            return 0;
+
+            //Best des
+            //return numbers.GroupBy(x => x).Single(x => x.Count() == 1).Key;
+        }
+    }
+}
--- KataCodWars/Program.cs
+++ KataCodWars/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using KataCodWars.Kata.Kata8kyu;
+//using KataCodWars.Kata.Kata8kyu;
 //using KataCodWars.Kata.Kata7kyu;
-//using KataCodWars.Kata.Kata6kyu;
+using KataCodWars.Kata.Kata6kyu;
 
 namespace KataCodWars
 {
@@ -11,13 +11,9 @@ namespace KataCodWars
     {
         static void Main(string[] args)
         {
-            var arr1 = new[] { 0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14 };
-            var arr2 = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15 };
-            
-            Kata7.CountPositivesSumNegatives(arr1);
-            Kata7.CountPositivesSumNegatives(arr2);
-            Kata7.CountPositivesSumNegatives(null);
-            Kata7.CountPositivesSumNegatives(new int[] { });
+            Kata7.GetUnique(new[] { 1, 2, 2, 2 });
+            Kata7.GetUnique(new[] { -2, 2, 2, 2 });
+            Kata7.GetUnique(new[] { 11, 11, 14, 11, 11 });
         }
     }
 }
