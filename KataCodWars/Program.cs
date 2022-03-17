c41fd3d3195f9ebd2af24c813b5595aa200a3564
IvanSminrov <includingivansmirnov@gmail.com>
2022-03-17 09:31:01 +0500

    add SumDigPower kata 6 kyu

--- KataCodWars/Program.cs
+++ KataCodWars/Program.cs
@@ -11,12 +11,11 @@ namespace KataCodWars
     {
         static void Main(string[] args)
         {
-            var res1 = Xbonacci.xbonacci(new double[] { 0, 1 }, 10);
-            var res2 = Xbonacci.xbonacci(new double[] { 1, 1 }, 10);
-            var res3= Xbonacci.xbonacci(new double[] { 0, 0, 0, 0, 1 }, 10);
-            
-            var res4= Xbonacci.xbonacci(new double[] { 1, 0, 0, 0, 0, 0, 1 }, 10);
-            var res5= Xbonacci.xbonacci(new double[] { 15, 0, 2, 19, 5, 0, 13, 0, 5, 7, 2, 8, 9, 10, 3, 16 }, 55);
+            var res1 = SumDigPower.SumDigPow(1, 10);
+            var res2 = SumDigPower.SumDigPow(1, 100);
+            var res3 = SumDigPower.SumDigPow(10, 100);
+            var res4 = SumDigPower.SumDigPow(90, 100);
+            var res5 = SumDigPower.SumDigPow(90, 150);
         }
     }
 }
