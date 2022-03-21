c7a13debf8340d2f30a488d4716439cee64a31cb
IvanSminrov <includingivansmirnov@gmail.com>
2022-03-21 09:10:04 +0500

    задача с округлением

---
+++ KataCodWars/Kata/Kata.6kyu/Kata 10.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace KataCodWars.Kata.Kata6kyu
+{
+    public class Kata10
+    {
+        public static double Solution(double n) => Math.Round(n * 2) / 2;
+    }
+}
--- KataCodWars/Program.cs
+++ KataCodWars/Program.cs
@@ -11,11 +11,14 @@ namespace KataCodWars
     {
         static void Main(string[] args)
         {
-            var res1 = SumDigPower.SumDigPow(1, 10);
-            var res2 = SumDigPower.SumDigPow(1, 100);
-            var res3 = SumDigPower.SumDigPow(10, 100);
-            var res4 = SumDigPower.SumDigPow(90, 100);
-            var res5 = SumDigPower.SumDigPow(90, 150);
+            var res1 = Kata10.Solution(4.2);
+            var res = Kata10.Solution(81.334575443218725);
+            var res2 = Kata10.Solution(4.3);
+            var res3 = Kata10.Solution(4.6);
+            var res4 = Kata10.Solution(4.7);
+            var res5 = Kata10.Solution(4.8);
+            var res6 = Kata10.Solution(4.5);
+            var res7 = Kata10.Solution(84.5);
         }
     }
 }
