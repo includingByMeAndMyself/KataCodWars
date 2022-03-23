7adc27bf4f42ecfa444ceec125e84a8c2f008d17
IvanSminrov <includingivansmirnov@gmail.com>
2022-03-23 10:33:29 +0500

    Tortoise  Race Kata 6

---
+++ KataCodWars/Kata/Kata.6kyu/Tortoise.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace KataCodWars.Kata.Kata6kyu
+{
+    public class Tortoise
+    {
+        public static int[] Race(int v1, int v2, int g)
+        {
+            if (v1 >= v2)
+                return null;
+            var ts = TimeSpan.FromSeconds((g * 3600) / (v2 - v1));
+            return new[] { ts.Hours, ts.Minutes, ts.Seconds };
+        }
+    }
+}
