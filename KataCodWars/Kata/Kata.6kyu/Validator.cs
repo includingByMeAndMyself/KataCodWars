aa5fa931a5af174542cbbfe4434baae84b7380e6
IvanSminrov <includingivansmirnov@gmail.com>
2022-04-26 16:52:26 +0500

    Kata6 IpAdress Validator

---
+++ KataCodWars/Kata/Kata.6kyu/Validator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataCodWars.Kata.Kata6kyu
+{
+    public class Validator
+    {
+
+        //public static bool is_valid_IP(string IpAddres)
+        //{
+        //    IPAddress ip;
+        //    bool validIp = IPAddress.TryParse(IpAddres, out ip);
+        //    return validIp && ip.ToString() == IpAddres;
+        //}
+
+
+        //public static bool is_valid_IP(string IpAddres)
+        //{
+        //    var octet = "([1-9][0-9]{0,2})";
+        //    var reg = $@"{octet}\.{octet}\.{octet}\.{octet}";
+        //    return new Regex(reg).IsMatch(IpAddres);
+        //}
+
+        public static bool is_valid_IP(string ipAddres)
+        {
+            if(string.IsNullOrWhiteSpace(ipAddres))
+                return false;
+
+            var numbs = ipAddres.Split('.');
+
+            if (numbs.Length != 4)
+                return false;
+
+            foreach (var num in numbs)
+            {
+                if (num.IndexOf(' ') >= 0)
+                    return false;
+
+                if (num.StartsWith("0") && num.Length > 1)
+                    return false;
+
+                if (!int.TryParse(num, out int obj))
+                    return false;
+                
+                if (obj > 255 || obj < 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
--- KataCodWars/Program.cs
+++ KataCodWars/Program.cs
@@ -11,8 +11,18 @@ namespace KataCodWars
     {
         static void Main(string[] args)
         {
-            var res = Kata12.IsPangram("The quick brown fox jumps over the lazy dog.");
+            var res = Validator.is_valid_IP("0.0.0.0");
+            var res1 = Validator.is_valid_IP("12.255.56.1");
+            var res2 = Validator.is_valid_IP("137.255.156.100");
 
+            var res3 = Validator.is_valid_IP("");
+            var res4 = Validator.is_valid_IP("abc.def.ghi.jkl");
+            var res6 = Validator.is_valid_IP("12.34.56");
+            var res5 = Validator.is_valid_IP("123.456.789.0");
+            var res7 = Validator.is_valid_IP("123.045.067.089");
+            var res8 = Validator.is_valid_IP("12.34.56.-1");
+            var res9 = Validator.is_valid_IP("12.34.56 .1");
+            var res10 = Validator.is_valid_IP("12.34.56.78sf");
         }
     }
 }
