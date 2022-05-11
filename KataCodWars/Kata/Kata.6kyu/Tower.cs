299b55f6c75e46984c9ff289dec91d9ee94cd242
IvanSminrov <includingivansmirnov@gmail.com>
2022-05-11 10:20:45 +0500

    TowerBuilder 6kyu

---
+++ KataCodWars/Kata/Kata.6kyu/Tower.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataCodWars.Kata.Kata6kyu
+{
+    public static class Tower
+    {
+        public static string[] TowerBuilder(int nFloors)
+        {
+            if(nFloors == 0) return null;
+
+            if(nFloors == 1) return new string[] { "*" };
+
+            var arr = new string[nFloors];
+            var floor = "";
+
+            for (int i = 0; i < nFloors; i++)
+            {
+                var stars = new String('*', i * 2 + 1);
+                var spaces = new String(' ', nFloors - i - 1);
+                floor = spaces.ToString() + stars + spaces;
+                arr[i] = floor;
+            }
+
+             
+            return arr;
+        }
+    }
+}
